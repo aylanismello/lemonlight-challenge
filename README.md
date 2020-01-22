83b2cd2dee95e2561fa77130e52396734b2ce5bb
Aylan Mello <aylanismello@gmail.com>
2020-01-21 17:06:47 -0800

    got slidedown to work

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 This simple layout was designed on Figma, with a live link [here](https://www.figma.com/file/6GpbXYVYDcG0V0woxDyhyi/Lemonlight-Challenge?node-id=0%3A1)
 
 ![alt text](https://res.cloudinary.com/burncartel/image/upload/v1579653594/professional%20stuff/lemonlight_mockup.png)
+
+
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,64 @@
 import React from "react";
-import './sidebar.css';
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
+import "./sidebar.css";
+import downArrow from "./chevron-down.svg";
 
-export default () => (<div className="Sidebar">
-  
-  
-</div>);
+const SidebarSection = ({ title, options, open, onSelect }) => (
+  <div className="Sidebar-Section">
+    <div className="Sidebar-Header" onClick={onSelect}>
+      <span className="Sidebar-Header-Text">{title}</span>
+      <img className="Sidebar-Arrow" src={downArrow} />
+    </div>
+    <SlideDown className={"my-dropdown-slidedown"}>
+      {open ? (
+        <div className="Sidebar-Links">
+          <ul>
+            {options.map(option => (
+              <li>{option}</li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </SlideDown>
+  </div>
+);
+
+class Sidebar extends React.Component {
+  state = {
+    openSidebarIdx: null
+  };
+
+  updateSidebarIdx(idx) {
+    if (idx === this.state.openSidebarIdx) {
+      this.setState({ openSidebarIdx: null });
+    } else {
+      this.setState({ openSidebarIdx: idx });
+    }
+  }
+
+  render() {
+    const { openSidebarIdx } = this.state;
+
+    return (
+      <div className="Sidebar">
+        <SidebarSection
+          title="Section 1"
+          options={[1, 2, 3]}
+          idx={0}
+          onSelect={() => this.updateSidebarIdx(0)}
+          open={openSidebarIdx === 0}
+        ></SidebarSection>
+        <SidebarSection
+          title="Section 2"
+          options={[4, 5, 6]}
+          idx={1}
+          open={openSidebarIdx === 1}
+          onSelect={() => this.updateSidebarIdx(1)}
+        ></SidebarSection>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
